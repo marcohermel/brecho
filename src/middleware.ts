58aa69ac961536d69c2816d76c4739e6d862e271
Marco Hermel <marco.hermel@avenuecode.com>
2023-06-09 11:58:39 -0300

    Revert "teste next middleware to redirect to https"
    This reverts commit bec960c4b5f0fa4e8cf663942b6d67fe4e0df6fe.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
+type Environment = "production" | "development" | "other";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    if (req.headers.get("x-forwarded-proto") !== "https") {
+    const currentEnv = process.env.NODE_ENV as Environment;
+    if (currentEnv === 'production' &&
+        req.headers.get("x-forwarded-proto") !== "https") {
         return NextResponse.redirect(
             `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
             301
