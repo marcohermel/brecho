c08f13ff6cb2da88ed06bf9737e7f4a0c37e7a77
Marco Hermel <marco.hermel@avenuecode.com>
2023-06-09 11:23:56 -0300

    test https redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect } from 'react'
 
 export default function Home() {
-
+  useEffect(() => {
+    if (location?.protocol !== 'https:') {
+      location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+  }, [])
   return (
     <div className={styles.divAll}>
       <Image src="/images/closet-do-bem.png" alt='brecho' width={600} height={800} />
