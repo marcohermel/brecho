945d1a8093ba266747f807ece5de1fe16f7e77fb
Marco Hermel <marco.hermel@avenuecode.com>
2023-06-19 02:05:31 -0300

    add https redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ import { useEffect } from 'react';
 
 export default function Home() {
   const size = useWindowSize();
-  // useEffect(() => {
-  //   if (location?.protocol !== 'https:') {
-  //     location.replace(`https:${location.href.substring(location.protocol.length)}`);
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (location?.protocol !== 'https:') {
+      location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+  }, [])
   if (size?.width <= MOBILE_LIMIT_WIDTH) {
   return <PageMobile />
   }
