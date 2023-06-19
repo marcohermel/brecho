7ed8001d92bcfbe403bc6cf44928940f118c0840
Marco Hermel <marco.hermel@avenuecode.com>
2023-06-19 01:36:53 -0300

    add ONGS

--- src/components/desktop/about-desktop.tsx
+++ src/components/desktop/about-desktop.tsx
@@ -3,12 +3,37 @@ import Image from 'next/image'
 import { ABOUT_TEXT } from '@/constants'
 
 export const AboutDesktop: React.FC = () => {
-    return (<section id="about" className='flex justify-between bg-tertiary py-10'>
+    return (<section id="about" className=''>
+        <div className='flex justify-between bg-tertiary py-10  text-secondary'>
+
         <div className='flex flex-col px-20'>
             <h3 className='text-center text-secondary w-full mb-4 border-b border-secondary'>SOBRE</h3>
-            <span className='text-secondary ml-4 text-center '>
+                <span className=' ml-4 text-justify'>
                 {ABOUT_TEXT}
             </span>
+                <span className=" text-center my-4">INSTITUIÇÕES ESCOLHIDAS PARA DOAÇÃO</span>
+                <div className='flex items-center justify-center gap-12'>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <Image
+                            src="/ONGS/ong-1.png"
+                            alt="ong-1"
+                            width={60}
+                            height={60}
+                            className="rounded-full"
+                        />
+                        <span className="text-center ">Sitio <br /> San Francesco</span>
+                    </div>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <Image
+                            src="/ONGS/ong-2.png"
+                            alt="ong-2"
+                            width={60}
+                            height={60}
+                            className="rounded-full"
+                        />
+                        <span className="text-center ">Instituto <br /> Lenon Joel pela Paz</span>
+                    </div>
+                </div>
         </div>
         <Image
             className='mr-40 rounded-2xl'
@@ -16,6 +41,7 @@ export const AboutDesktop: React.FC = () => {
             alt="logo"
             width={567}
             height={567}
-        />
+            />
+        </div>
     </section>)
 }
--- src/components/mobile/about-mobile.tsx
+++ src/components/mobile/about-mobile.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { ABOUT_TEXT } from '@/constants';
 
 export const AboutMobile: React.FC = () => {
-    return (<section id="about" className='flex flex-col  w-full items-center bg-tertiary'>
+    return (
+        <section id="about" className='flex flex-col w-full items-center bg-tertiary'>
         <h2 className="text-secondary py-4">SOBRE</h2>
         <div className="flex flex-col pt-4 pb-4 w-full items-center bg-secondary">
             <Image
@@ -12,8 +13,32 @@ export const AboutMobile: React.FC = () => {
                 width={368}
                 height={223}
             />
-            <span className="text-white text-center my-4">{ABOUT_TEXT}</span>
-        </div>
+                <span className="text-white text-justify mx-2 my-4 mb-10">{ABOUT_TEXT}</span>
+
+                <span className="text-white text-center my-4 mb-10">INSTITUIÇÕES ESCOLHIDAS PARA DOAÇÃO</span>
+                <div className='flex items-center justify-between gap-12'>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <Image
+                            src="/ONGS/ong-1.png"
+                            alt="ong-1"
+                            width={60}
+                            height={60}
+                            className="rounded-full"
+                        />
+                        <span className="text-center">Sitio <br /> San Francesco</span>
+                    </div>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <Image
+                            src="/ONGS/ong-2.png"
+                            alt="ong-2"
+                            width={60}
+                            height={60}
+                            className="rounded-full"
+                        />
+                        <span className="text-center">Instituto <br /> Lenon Joel pela Paz</span>
+                    </div>
+                </div>
+            </div>
     </section>
     );
 }
